9162e2df01a5a0270b5677f7a9d8d869d3fb25ec
PaulitoRenatito <paulorenatosouza02@gmail.com>
2024-03-11 17:32:49 -0300

    Favourites context

---
+++ src/services/favourites/favourites.context.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface FavouritesContextProps {
+    favourites: any | null;
+    addToFavourites: (restaurant: any) => void;
+    removeFromFavourites: (restaurant: any) => void;
+};
+export const FavouritesContext = createContext<FavouritesContextProps | null>(null);
+
+interface FavouritesContextProviderProps {
+    children: ReactNode,
+}
+export const FavouritesContextProvider = ({ children }: FavouritesContextProviderProps) => {
+
+    const [favourites, setFavourites] = useState<any>([]);
+
+    const add = (restaurant: any) => {
+        setFavourites([...favourites, restaurant]);
+    }
+
+    const remove = (restaurant: any) => {
+        const newFavourites = favourites.filter(
+            (x: any) => x.placeId !== restaurant.placeId
+        );
+
+        setFavourites(newFavourites);
+    }
+
+    return (
+        <FavouritesContext.Provider
+            value={{
+                favourites,
+                addToFavourites: add,
+                removeFromFavourites: remove,
+            }}
+        >
+            {children}
+        </FavouritesContext.Provider>
+    );
+}
